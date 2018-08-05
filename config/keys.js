a703f76f4ec9b6d88807fde7fe4d32c045e78086
Shawn Stewart <s.stewart0220@gmail.com>
2018-08-05 14:50:14 -0400

    Created JWT secret key

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
 module.exports = {
-  mongoURI: "mongodb://Shawn:Password123@ds113522.mlab.com:13522/mern-stack-app"
+  mongoURI:
+    "mongodb://Shawn:Password123@ds113522.mlab.com:13522/mern-stack-app",
+  jwtSecret: "superdupersecretkey"
 };
