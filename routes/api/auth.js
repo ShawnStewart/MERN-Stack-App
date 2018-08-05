ba52149382771d1b389a69e8c37c611c5c5b80e8
Shawn Stewart <s.stewart0220@gmail.com>
2018-08-05 15:52:29 -0400

    Created a protected route
    Created a route that requires a jsonwebtoken to access.

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -5,6 +5,7 @@ const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
+const passport = require("passport");
 
 // @route   GET api/users/test
 // @desc    Tests users route
@@ -78,5 +79,21 @@ router.post("/login", (req, res) => {
     });
   });
 });
+
+// @route   GET api/users/current
+// @desc    Return current user
+// @access  Private
+router.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email,
+      avatar: req.user.avatar
+    });
+  }
+);
 
 module.exports = router;
