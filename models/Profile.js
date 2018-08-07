4052c7dfb1382d757aef39add3d6c6e5d542a06b
Shawn Stewart <s.stewart0220@gmail.com>
2018-08-07 14:38:23 -0400

    Created Profile model
    Created the Profile schema

---
+++ models/Profile.js
@@ -0,0 +1,119 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const ObjectID = Schema.type.ObjectID;
+
+// Create Schema
+const ProfileSchema = new Schema({
+  user: {
+    type: ObjectID,
+    refs: "users"
+  },
+  handle: {
+    type: String,
+    required: true,
+    max: 40
+  },
+  company: {
+    type: String
+  },
+  website: {
+    type: String
+  },
+  location: {
+    type: String
+  },
+  status: {
+    type: String,
+    required: true
+  },
+  skills: {
+    type: [String],
+    required: true
+  },
+  bio: {
+    type: String
+  },
+  githubUsername: {
+    type: String
+  },
+  experience: [
+    {
+      title: {
+        type: String,
+        required: true
+      },
+      company: {
+        type: String,
+        required: true
+      },
+      location: {
+        type: String
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
+      }
+    }
+  ],
+  education: [
+    {
+      school: {
+        type: String,
+        required: true
+      },
+      degree: {
+        type: String,
+        required: true
+      },
+      fieldofstudy: {
+        type: String,
+        required: true
+      },
+      from: {
+        type: Date,
+        required: true
+      },
+      to: {
+        type: Date
+      },
+      current: {
+        type: Boolean,
+        default: false
+      },
+      description: {
+        type: String
+      }
+    }
+  ],
+  social: {
+    twitter: {
+      type: string
+    },
+    linkedin: {
+      type: string
+    },
+    instagram: {
+      type: string
+    },
+    youtube: {
+      type: string
+    }
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+    required: true
+  }
+});
+
+module.exports = Profile = mongoose.model("profile", ProfileSchema);
